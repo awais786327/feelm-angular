c72b8cccaa76fac0f3a2663d4826ca16c7e295ef
awais786327 <awais3227443@gmail.com>
2020-08-31 00:37:43 +0500

    angular pwa

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,41 +1,44 @@
-import { Component, OnInit } from '@angular/core';
-import { finalize } from 'rxjs/operators';
+import { Component, AfterViewInit } from '@angular/core';
+import axios from 'axios';
 
-import { QuoteService } from './quote.service';
-import data from '../../assets/address_endpoint_response.json';
+declare var Swiper:any;
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  quote: string | undefined;
+export class HomeComponent implements AfterViewInit {
   isLoading = false;
-  isResult: any = null;
-  items: any = null;
 
-  constructor(private quoteService: QuoteService) {
-    this.items = data;
-    console.log(data);
+  constructor() {
   }
 
-  ngOnInit() {
-    // this.quoteService.getRandomQuote({ category: 'dev' })
-    //   .pipe(finalize(() => { this.isLoading = false; }))
-    //   .subscribe((quote: string) => { this.quote = quote; });
-  }
-
-  showResult(val: string) {
-    this.isResult = '';
-    this.isLoading = true;
-    setTimeout(() => {
-      this.isLoading = false;
-      this.isResult = val;
+  ngAfterViewInit() {
+    const el = '.swiper-container';
+    const swiper = new Swiper(el, {
+      // centeredSlides: true,
+      direction: 'vertical',
+      loop: true,
+      infinite: true,
+      freeMode: true,
+      slidesPerView: 5,
+      slidesToScroll: 1,
+      keyboard: {
+        enabled: true
+      },
+      mousewheel: {
+        releaseOnEdges: true
+      },
+      freeModeSticky: true
     });
   }
 
-  getPhones(phones: any) {
-    return phones.map((obj: any) => obj.phone_number);
+  openNav() {
+    document.getElementById('menu').style.height = '100%';
+  }
+
+  closeNav() {
+    document.getElementById('menu').style.height = '0%';
   }
 }
